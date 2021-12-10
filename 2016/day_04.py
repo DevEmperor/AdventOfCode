c670df2391eaa5342a0e404447b93987547cfa6a
DevEmperor <56255079+DevEmperor@users.noreply.github.com>
2021-12-10 17:42:14 +0100

    Added 2016 / 04 / 1 & 2

--- 2016/day_04.py
+++ 2016/day_04.py
@@ -1,14 +1,19 @@
 from collections import Counter
+from string import ascii_letters
 
 with open("text_04.txt") as file:
     data = file.readlines()
 data = [d.strip() for d in data]
 
-data = ["aaaaa-bbb-z-y-x-123[abxyz]"]
+s = 0
+abc = ascii_letters[:26]
 for room in data:
     mcl = room.split("[")[1][:-1]
     sid = int(room.split("-")[-1].split("[")[0])
     name = "".join(room.split("-")[:-1])
-    c = Counter(name).most_common(5)
-
-    print(c); exit()
+    name = "".join(abc[(abc.index(c) + sid) % 26] for c in name)  # remove for part 1
+    if name == "northpoleobjectstorage": print(sid); exit()  # remove for part 1
+    c = Counter(sorted(name)).most_common()
+    if all(c[idx][0] == mcl[idx] for idx in range(len(mcl))):
+        s += sid
+print(s)
